839d99fd96f5632280af82b3f6ab0f1f859456d8
EraSend <117540807+EraSend@users.noreply.github.com>
2022-11-07 17:14:32 +0300

    Update BaseClassScript.cs

--- Scripts/BaseClassScript.cs
+++ Scripts/BaseClassScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Config class
 public class BaseClassScript : MonoBehaviour
 {
     public static float PlayerSpeed = 4f;
