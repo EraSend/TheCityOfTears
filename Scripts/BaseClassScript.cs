5a392cb3e1c14ebc178ef00bd68f6361cb6d8532
EraSend <117540807+EraSend@users.noreply.github.com>
2022-11-07 17:12:50 +0300

    Add files via upload

---
+++ Scripts/BaseClassScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClassScript : MonoBehaviour
+{
+    public static float PlayerSpeed = 4f;
+    public static float JumpForce = 7f;
+    public static bool PlayerRight = true;
+}
