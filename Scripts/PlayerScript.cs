3fe857c031dfb655d39366111a166c6ba9b548ce
EraSend <117540807+EraSend@users.noreply.github.com>
2022-11-07 17:10:45 +0300

    Update PlayerScript.cs

--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 // Made by Snowly
@@ -7,23 +8,25 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private float speed = 0.5f;
-    private float JumpForce = 7f;
 
     private void Start()
     {
-        rb = GetComponet<Rigidbody2D>();
-        sr = GetComponet<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        float movement = Input.GetAxis("Hotizontal");
-        transform.position += new Vector3(movement, 0, 0) * speed * Time.delta.Time;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localPosition += -transform.right * BaseClassScript.PlayerSpeed * Time.deltaTime;
+            sr.flipX = false;
+        }
 
-        if(Input.GetKeyDown(KeyCode.Space) && MAthf.Abs(rb.velocity.y) <0.05f)
-          {rb.AddForce(new Vector2(0, 1));}
-
-          sr.flipX = movement < 0 ? true : false;
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.localPosition += transform.right * BaseClassScript.PlayerSpeed * Time.deltaTime;
+            sr.flipX = true;
+        }
     }
 }
